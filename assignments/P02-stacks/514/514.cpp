086d8e3245726becd618daae5ec20fba14dfa175
paigechampagne <paigechamp@gmail.com>
2020-12-07 03:14:05 -0600

    n

--- assignments/P02-stacks/514/514.cpp
+++ assignments/P02-stacks/514/514.cpp
@@ -15,62 +15,108 @@
 // Files:           514.cpp
 //                  input
 /////////////////////////////////////////////////////////////////////////////////
-
 #include <iostream>
-#include <stack>
-
+#include <algorithm> 
+#include <vector>
+#include <string>
+#define endl "\n"
 using namespace std;
-
-int main()
-{
-    int n, x;
-
-    while (1)
-    {
-        stack<int> stack1, stack2;
-        scanf("%d", &n);
-        if (n == 0)
-            break;
-
-        while (1)
-        {
-            int i = n;
-            scanf("%d", &x);
-            if (x == 0)
-            {
-                printf("\n");
-                break;
+// void printVector(vector<int> v){
+//     for (int i = 0; i < v.size(); i++){
+//         cout << v[i] << " ";
+//     }
+//     cout<<endl;
+// }
+/**
+ * 514 Rails
+ */
+int main(int argc, char** argv){
+    int numCars;
+    int carNum;
+    int carCount=0;
+    int a;
+    bool keepChecking = true;
+    int sum;
+    vector<int> outTrain;
+    vector<int> inTrain;
+    vector<int> station;
+    cin>>numCars;
+    inTrain.clear();
+    outTrain.clear();
+    station.clear();
+    a=1;
+    while(numCars){
+        cin>>carNum;
+        if(carNum == 0){
+            cout<<endl;
+            outTrain.clear();
+            carCount = 0;
+            cin>>numCars;
+            if(numCars == 0){
+                exit(0);
             }
-
-            stack1.push(x);
-
-            for (int i = 1; i < n; i++)
-            {
-                scanf("%d", &x);
-                stack1.push(x);
-            }
-
-            for (; i >= 1;)
-            {
-                if (!stack2.empty() && stack2.top() == i)
-                {
-                    stack2.pop();
-                    i--;
-                    continue;
+        } else {
+            inTrain.push_back(a);
+            outTrain.push_back(carNum);
+            carCount++;
+            a++;
+        }
+        if(carCount % numCars == 0){
+            station.clear();
+            if(inTrain.size()){
+                if(inTrain == outTrain){
+                    cout<<"Yes";
+                }else if(inTrain.size() != outTrain.size()){
+                    cout<<"No";
+                }else{
+                    
+                    int i = 0;
+                    int j = 0;
+                    keepChecking = true;
+                    while(keepChecking){
+                        if(i < numCars){
+                            if(inTrain[i] == outTrain[j]){
+                                i++;
+                                j++;
+                            }else if(station.size() > 0 && station.back() == outTrain[j]){
+                                station.pop_back();
+                                j++;
+                            }else{
+                                station.push_back(inTrain[i]);
+                                i++;
+                            }
+                        }else{
+                            if(j>=numCars){
+                                keepChecking = false;
+                                cout<<"Yes";
+                                i=0;
+                                j=0;
+                                break;
+                            }else if(station.size() > 0 && station.back() == outTrain[j]){
+                                station.pop_back();
+                                j++;
+                            }else{
+                                keepChecking = false;
+                                cout<<"No";
+                                break;
+                            }
+                        }
+                    }
+                    /*cout<<"\t : ";
+                    sum = 0;
+                    for(int i=0;i<inTrain.size();i++){
+                        cout<<inTrain[i]-outTrain[i]<<"\t";
+                    }
+                    for(int i=0;i<inTrain.size()/2+1;i++){
+                        sum += (inTrain[i]-outTrain[i]);
+                    }               
+                    cout<<sum;*/
                 }
-
-                if (stack1.empty())
-                    break;
-                if (stack1.top() != i)
-                    stack2.push(stack1.top());
-                else
-                    i--;
-                stack1.pop();
+                cout<<endl;
             }
-            if (stack1.empty() && stack2.empty())
-                cout << "Yes\n";
-            else
-                cout << "No\n";
+            inTrain.clear();
+            outTrain.clear();
+            a = 1;
         }
     }
     return 0;
--- assignments/P03-Queues/11988/11988.cpp
+++ assignments/P03-Queues/11988/11988.cpp
@@ -15,46 +15,49 @@
 // Files:           127.cpp
 //                  input
 /////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
-#include <deque>
+#include <list>
+#include <iterator>
 using namespace std;
 
 int main() {
   
   char a;
   string s;
-  char holder;
  while(getline(cin, s)){
-  deque <char> list;
-  deque <char> parenthesis;
-   string hold;
+  list <char> list, parenthesis;
+  string hold;
+  int j = 0;
    for(int i = 0; i < s.length(); i++){
-     if(s[i] =='['){
+              //if regular letters
+     if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] == '_')){
+       if(parenthesis.empty()){ //if there is no beiju
+         list.push_back(s[i]);
+       }else{// if there's [] on the list and s[i] isn't a [], put beiju in holder
+         hold += s[i];
+       }
+     }else if(s[i] =='['){
        parenthesis.push_back('[');
-
-     }
-      if(s[i] == ']'){
+     }else if(s[i] == ']'){
         parenthesis.pop_front();
-      }
-
-    if(!parenthesis.empty() && s[i] != '[' && s[i] != ']'){
-      hold += s[i];
-    }
-
-    if(parenthesis.empty() && s[i] != ']' && s[i] != '[')
-      list.push_back(s[i]);
+     }  
 
    }
+  //cout << hold[0] <<endl;
    if(hold.size() > 0){
       for(int i = hold.size(); i >= 0; i--){
-     list.push_front(hold[i]);
+        list.push_front(hold[i]);
       }
-   }
-    for(int i = 0; i < list.size(); i++){
-        cout << list[i];
+    }
+    int k = list.size();
+    for(int i = 0; i < k; i++){
+        cout << list.front();
+        list.pop_front();
       }
       cout << "\n";
  }
 
 
 }
+
